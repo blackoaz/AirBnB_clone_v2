ee05c41e5055c7d3f2da1fdbfff70fcbd61e5053
blackoaz <pauloakello56@gmail.com>
2023-07-09 08:10:29 -0700

    adding updates to fabric file for transferring resources to remote server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -35,7 +35,7 @@ echo
 
 #--create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR newuser:newuser /data
+sudo chown -hR ubuntu:ubuntu /data
 
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,8 +6,8 @@ import shlex
 import os
 
 
-env.hosts = ['100.26.18.154', '54.175.95.164']
-env.user = "newuser"
+env.hosts = ['54.175.95.164','100.26.18.154']
+env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
@@ -27,13 +27,13 @@ def do_deploy(archive_path):
         tmp_path = "/tmp/{}".format(name)
 
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
+        sudo("mkdir -p {}".format(releases_path))
+        sudo("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        sudo("rm {}".format(tmp_path))
+        sudo("mv {}web_static/* {}".format(releases_path, releases_path))
+        sudo("rm -rf {}web_static".format(releases_path))
+        sudo("rm -rf /data/web_static/current")
+        sudo("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
     except:
